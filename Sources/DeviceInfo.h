150f2211bb3544812ca86cd7a19a39cb6a7bd874
Ben Norris <ben@bsn.io>
2016-03-25 11:29:20 -0700

    Add blank framework

---
+++ Sources/DeviceInfo.h
@@ -0,0 +1,19 @@
+//
+//  DeviceInfo.h
+//  DeviceInfo
+//
+//  Created by Ben Norris on 3/25/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeviceInfo.
+FOUNDATION_EXPORT double DeviceInfoVersionNumber;
+
+//! Project version string for DeviceInfo.
+FOUNDATION_EXPORT const unsigned char DeviceInfoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceInfo/PublicHeader.h>
+
+
