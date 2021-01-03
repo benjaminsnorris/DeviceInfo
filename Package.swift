de27e4fee9abfb1b22928990e4abdf85c0b0667c
Ben Norris <code@bsn.design>
2021-01-02 19:46:56 -0700

    Remove watchOS target from manifest

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DeviceInfo",
-            targets: ["DeviceInfo", "DeviceInfo-watchOS"]),
+            targets: ["DeviceInfo"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
