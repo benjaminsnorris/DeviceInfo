7c6dc23994607625d4fc0f0a340d3ed7ce94f9e0
Ben Norris <code@bsn.design>
2021-01-02 19:45:42 -0700

    Fix target names in manifest

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DeviceInfo",
-            targets: ["DeviceInfo"]),
+            targets: ["DeviceInfo", "DeviceInfo-watchOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,7 +24,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "DeviceInfo",
+            name: "DeviceInfoTests",
             dependencies: []),
         .testTarget(
             name: "DeviceInfo",
