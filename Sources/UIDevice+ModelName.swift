9b7a3b9cf6cfbebf1dd6344343409870b9a41fd3
Ben Norris <code@bsn.design>
2021-01-02 19:31:22 -0700

    Update for new models

--- Sources/UIDevice+ModelName.swift
+++ Sources/UIDevice+ModelName.swift
@@ -32,10 +32,19 @@ extension UIDevice {
 
 // https://www.theiphonewiki.com/wiki/Models
 private let DeviceList = [
+    
+    // MARK: - iPod
+    
     /* iPod 5 */
     "iPod5,1": "iPod Touch 5",
     /* iPod 6 */
     "iPod7,1": "iPod Touch 6",
+    /* iPod 7 */
+    "iPod9,1": "iPod Touch 7",
+    
+    
+    // MARK: - iPhone
+    
     /* iPhone 4 */
     "iPhone3,1":  "iPhone 4", "iPhone3,2": "iPhone 4", "iPhone3,3": "iPhone 4",
     /* iPhone 4S */
@@ -66,6 +75,32 @@ private let DeviceList = [
     "iPhone10,2": "iPhone 8 Plus", "iPhone10,5": "iPhone 8 Plus",
     /* iPhone X */
     "iPhone10,3": "iPhone X", "iPhone10,6": "iPhone X",
+    /* iPhone XR */
+    "iPhone11,8": "iPhone XR",
+    /* iPhone XS */
+    "iPhone11,2": "iPhone XS",
+    /* iPhone XS Max */
+    "iPhone11,6": "iPhone XS Max", "iPhone11,4": "iPhone XS Max",
+    /* iPhone 11 */
+    "iPhone12,1": "iPhone 11",
+    /* iPhone 11 Pro */
+    "iPhone12,3": "iPhone 11 Pro",
+    /* iPhone 11 Pro Max */
+    "iPhone12,5": "iPhone 11 Pro Max",
+    /* iPhone SE (2nd generation) */
+    "iPhone12,8": "iPhone SE (2nd generation)",
+    /* iPhone 12 mini */
+    "iPhone13,1": "iPhone 12 mini",
+    /* iPhone 12 */
+    "iPhone13,2": "iPhone 12",
+    /* iPhone 12 Pro */
+    "iPhone13,3": "iPhone 12 Pro",
+    /* iPhone 12 Pro Max */
+    "iPhone13,4": "iPhone 12 Pro Max",
+    
+    
+    // MARK: - iPad
+    
     /* iPad 2 */
     "iPad2,1": "iPad 2", "iPad2,2": "iPad 2", "iPad2,3": "iPad 2", "iPad2,4": "iPad 2",
     /* iPad 3 */
@@ -74,26 +109,57 @@ private let DeviceList = [
     "iPad3,4": "iPad 4", "iPad3,5": "iPad 4", "iPad3,6": "iPad 4",
     /* iPad 5th Generation */
     "iPad6,11": "iPad 5", "iPad6,12": "iPad 5",
+    /* iPad 6th Generation */
+    "iPad7,5": "iPad 6", "iPad7,6": "iPad 6",
+    /* iPad 7th Generation */
+    "iPad7,11": "iPad 7", "iPad7,12": "iPad 7",
+    /* iPad 8th Generation*/
+    "iPad11,6": "iPad 8", "iPad11,7": "iPad 8",
+    
+    
+    // MARK: - iPad Air
+    
     /* iPad Air */
     "iPad4,1": "iPad Air", "iPad4,2": "iPad Air", "iPad4,3": "iPad Air",
     /* iPad Air 2 */
     "iPad5,3": "iPad Air 2", "iPad5,4": "iPad Air 2",
+    /* iPad Air 3rd Generation */
+    "iPad11,3": "iPad Air 3", "iPad11,4": "iPad Air 3",
+    /* iPad Air 4th Generation */
+    "iPad13,1": "iPad Air 4", "iPad13,2": "iPad Air 4",
     /* iPad Mini */
     "iPad2,5": "iPad Mini", "iPad2,6": "iPad Mini", "iPad2,7": "iPad Mini",
     /* iPad Mini 2 */
-    "iPad4,4": "iPad Mini", "iPad4,5": "iPad Mini", "iPad4,6": "iPad Mini 2",
+    "iPad4,4": "iPad Mini 2", "iPad4,5": "iPad Mini 2", "iPad4,6": "iPad Mini 2",
     /* iPad Mini 3 */
-    "iPad4,7": "iPad Mini", "iPad4,8": "iPad Mini", "iPad4,9": "iPad Mini 3",
+    "iPad4,7": "iPad Mini 3", "iPad4,8": "iPad Mini 3", "iPad4,9": "iPad Mini 3",
     /* iPad Mini 4 */
-    "iPad5,1": "iPad Mini", "iPad5,2": "iPad Mini",
+    "iPad5,1": "iPad Mini 4", "iPad5,2": "iPad Mini 4",
+    /* iPad Mini 5 */
+    "iPad11,1": "iPad Mini 5", "iPad11,2": "iPad Mini 5",
+    
+    
+    // MARK: - iPad Pro
+    
     /* iPad Pro (12.9 inch) */
-    "iPad6,7": "iPad Pro (12.9 inch) 2nd Generation", "iPad6,8": "iPad Pro (12.9 inch) 2nd Generation",
-    /* iPad Pro (12.9 inch) 2nd Generation */
-    "iPad7,1": "iPad Pro (12.9 inch)", "iPad7,2": "iPad Pro (12.9 inch)",
+    "iPad6,7": "iPad Pro (12.9 inch)", "iPad6,8": "iPad Pro (12.9 inch)",
     /* iPad Pro (9.7 inch) */
     "iPad6,3": "iPad Pro (9.7 inch)", "iPad6,4": "iPad Pro (9.7 inch)",
+    /* iPad Pro (12.9 inch) 2nd generation */
+    "iPad7,1": "iPad Pro (12.9 inch) (2nd generation)", "iPad7,2": "iPad Pro (12.9 inch) (2nd generation)",
     /* iPad Pro (10.5 inch) */
     "iPad7,3": "iPad Pro (10.5 inch)", "iPad7,4": "iPad Pro (10.5 inch)",
+    /* iPad Pro (11 inch) */
+    "iPad8,1": "iPad Pro (11 inch)", "iPad8,2": "iPad Pro (11 inch)", "iPad8,3": "iPad Pro (11 inch)", "iPad8,4": "iPad Pro (11 inch)",
+    /* iPad Pro (12.9 inch) (3rd generation) */
+    "iPad8,5": "iPad Pro (12.9 inch) (3rd generation)", "iPad8,6": "iPad Pro (12.9 inch) (3rd generation)", "iPad8,7": "iPad Pro (12.9 inch) (3rd generation)", "iPad8,8": "iPad Pro (12.9 inch) (3rd generation)",
+    /* iPad Pro (11 inch) (2nd generation) */
+    "iPad8,9": "iPad Pro (11 inch) (2nd generation)", "iPad8,10": "iPad Pro (11 inch) (2nd generation)",
+    /* iPad Pro (12.9 inch) (4th generation) */
+    "iPad8,11": "iPad Pro (12.9 inch) (4th generation)", "iPad8,12": "iPad Pro (12.9 inch) (4th generation)",
+    
+    // MARK: - Simulator
+    
     /* Simulator */
     "x86_64": "Simulator", "i386": "Simulator"
 ]
